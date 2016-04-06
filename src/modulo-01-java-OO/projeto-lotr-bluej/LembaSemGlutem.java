53090715142fd9345e89ea2248cb83548be4cdda
Fernando Schneider Leandro <fernando.leandro@cwinet.local>
2016-04-06 13:23:08 -0300

    fix(correções): correção na classe LembasSemGlutem.

--- src/modulo-01-java-OO/projeto-lotr-bluej/LembaSemGlutem.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/LembaSemGlutem.java
@@ -9,23 +9,21 @@ public class LembaSemGlutem
 {
     // instance variables - replace the example below with your own
     private int quantLembas;
-    private int numPersonagens;
+    
 
     /**
      * Constructor for objects of class LembaSemGlutem
      */
-    public LembaSemGlutem(int quantLembas, int numPersonagens)
+    public LembaSemGlutem(int quantLembas)
     {
         // initialise instance variables
         this.quantLembas = quantLembas;
-        this.numPersonagens = numPersonagens;
+        
     }
 
     public boolean podeDividirPares(){
         
-        if((this.quantLembas >= 1) && (this.quantLembas <= 100))
-            return((this.quantLembas % 2 == 0) && (this.quantLembas >= 2* numPersonagens));
-        
-        return false;    
+        return((this.quantLembas % 2 == 0) && (this.quantLembas > 2) && (this.quantLembas <= 100));
+            
     }
 }
