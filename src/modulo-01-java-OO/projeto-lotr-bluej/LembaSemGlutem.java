ae964da457bd934a3461c6c0be81c9d338b87cfd
FernandoSL <nando.leandro@hotmail.com>
2016-04-06 00:15:26 -0300

    Update LembaSemGlutem.java

--- src/modulo-01-java-OO/projeto-lotr-bluej/LembaSemGlutem.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/LembaSemGlutem.java
@@ -8,22 +8,24 @@
 public class LembaSemGlutem
 {
     // instance variables - replace the example below with your own
-    private int quant;
+    private int quantLembas;
+    private int numPersonagens;
 
     /**
      * Constructor for objects of class LembaSemGlutem
      */
-    public LembaSemGlutem(int quant)
+    public LembaSemGlutem(int quantLembas, int numPersonagens)
     {
         // initialise instance variables
-        this.quant = quant;
+        this.quantLembas = quant;
+        this.numPersonagens = numPersonagens;
     }
 
     public boolean podeDividirPares(){
         
         boolean result = false;
         
-        if((this.quant % 2 == 0) && (this.quant != 2))
+        if((this.quant % 2 == 0) && (this.quant >= 2* numPersonagens))
             result = true;
         
         return result; 
