563775d45ae1e8eb3401205b3f01c8ea577b7bb5
Fernando Schneider Leandro <Fernando Schneider Leandro>
2016-05-19 21:24:04 -0300

    feat(IRON): Implementar html que carregue as imagens dos albuns do Iron Maiden utilizando ajax.

---
+++ src/modulo-06-ajax/Tema01/spotfy-iron.js
@@ -0,0 +1,11 @@
+'use strict'
+var teste;
+
+(function(){
+  $.ajax({ url : 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50', type: 'GET'}).done(function(retorno){
+    for (var i = 0; i < retorno.items.length; i++) {
+      var url = retorno.items[i].images[0].url;
+      $('<img>').attr('src', url).appendTo($('#container-imgs'));
+    }
+  })
+})();
