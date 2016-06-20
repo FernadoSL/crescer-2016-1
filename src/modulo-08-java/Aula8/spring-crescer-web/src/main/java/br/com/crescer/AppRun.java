136f20c4e0f0b87900aceb08eda18a3a66d1f5cf
Fernando Schneider Leandro <nando.leandro@hotmail.com>
2016-06-20 16:30:54 -0300

    feat(Aula 8): implementar projeto que realiza crud utilizando spring.

--- src/modulo-08-java/Aula8/spring-crescer-web/src/main/java/br/com/crescer/AppRun.java
+++ src/modulo-08-java/Aula8/spring-crescer-web/src/main/java/br/com/crescer/AppRun.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.cwi;
+package br.com.crescer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
- * @author fernando.leandro
+ * @author Carlos H. Nonnemacher
  */
 @SpringBootApplication
 public class AppRun {
